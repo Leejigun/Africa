0f897459174c7651a1df6306d2fd388ae93dd71e
bimo.ez <bimo.ez@kakaomobility.com>
2022-10-17 00:20:16 +0900

    그리드 뷰 추가

--- Screens/GalleryView.swift
+++ Screens/GalleryView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    @State private var selectedAnimal: Animal?
+    @State private var gridColumn: Float = 3
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    
+    private let animals: [Animal] = Bundle.main.decode(from: .animals) ?? []
+    private let haptic = UIImpactFeedbackGenerator(style: .medium)
+    
+    private func switchGrid() {
+        gridLayout = Array(repeating: GridItem(.flexible()), count: Int(gridColumn))
+    }
+    
     var body: some View {
-        Text("GalleryView")
+        ScrollView(.vertical, showsIndicators: true) {
+            
+            VStack(alignment: .center, spacing: 30) {
+                Image(selectedAnimal?.image ?? "logo")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                    .padding(.horizontal, 30)
+                
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding()
+                    .onChange(of: gridColumn) { _ in
+                        switchGrid()
+                    }
+                
+                LazyVGrid(columns: gridLayout, alignment: .center) {
+                    
+                    ForEach(animals) { animal in
+                        Image(animal.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = animal
+                                haptic.impactOccurred()
+                            }
+                    }
+                }
+            }
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(MotionAnimationView())
+        .onAppear {
+            switchGrid()
+        }
     }
 }
 
